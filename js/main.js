83f17d47ab53734ee0d6c3f0a1ae7d87b2053d4f
Sergi <sergicasanova6798@gmail.com>
2023-10-30 13:51:04 +0100

    feat(contenidoCSS): Contenido css realizado

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function changeCSS() {
+    document.getElementById('demo').style.backgroundColor = "black";
+    document.getElementById('demo').style.color = "white";
+    document.getElementById('demo').style.fontSize = "40px";
+}
