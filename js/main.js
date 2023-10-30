26b85529907c4c730758aa43856b1edfd23aa85c
Sergi <sergicasanova6798@gmail.com>
2023-10-30 17:16:36 +0100

    feat(contenidoCSS): Botones bien configurados

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function changeCSS() {
+function cambioCSS() {
     document.getElementById('demo').style.backgroundColor = "black";
     document.getElementById('demo').style.color = "white";
     document.getElementById('demo').style.fontSize = "40px";
